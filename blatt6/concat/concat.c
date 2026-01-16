17eb276e063c6a0a75e484c2277da05ab0179afd
Sören Tempel <tempel@ibr.cs.tu-bs.de>
2026-01-16 12:50:35 +0000

    Vorgabe für die Aufgabe 6 [ci skip]

---
+++ blatt6/concat/concat.c
@@ -0,0 +1,21 @@
+/* TODO: includes */
+#define READ 0  // Lesendes Ende einer pipe(3)
+#define WRITE 1 // Schreibendes Ende einer pipe(3)
+
+/* TODO: globals */
+
+/* TODO: helpers */
+
+
+/* seq 1 MAX | awk AWK_VORSCHRIFT | grep GREP_VORSCHRIFT > OUTPUT_FILE */
+
+int main(int argc, char *argv[]) {
+	return 1;
+/* TODO: implement me */
+	// SEQ_NOT_IMPLEMENTED_MARKER remove this marker to activate seq related testcases
+	// AWK_NOT_IMPLEMENTED_MARKER remove this marker to activate awk related testcases
+	// GREP_NOT_IMPLEMENTED_MARKER remove this marker to activate grep related testcases
+	// OUTPUT_NOT_IMPLEMENTED_MARKER remove this marker to activate output redirection related testcases
+}
+
+
---
+++ blatt6/ticker/ticker.c
@@ -0,0 +1,17 @@
+/* TODO: includes */
+
+/* TODO: global variables */
+
+/*
+ * Fehlerbehandlung weggelassen bei:
+ * 		- sigaction -> Fehler kann nur durch eigene Argumente verursacht werden.
+ * 		- sigemptyset -> "
+ * 		- gettimeofday -> " */
+
+/* TODO: helpers, signal handlers */
+
+/* TODO: implement main */
+/* ARGUMENT_NOT_IMPLEMENTED_MARKER remove to activate argument parsing tests */
+/* BASICA_NOT_IMPLEMENTED_MARKER remove to activate simple straight forward test runs without SIGQUIT and without testing for correct timestamps */
+/* BASICB_NOT_IMPLEMENTED_MARKER remove to activate simple straight forward test runs without SIGQUIT */
+/* SIGQUIT_NOT_IMPLEMENTED_MARKER remove to enable testaces which send SIGTERM to stop a race */
